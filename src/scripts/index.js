44f5a606709bfe4732f32262d59590be68b4270b
Jamie Dixon <github@jamie-dixon.co.uk>
2015-10-24 20:22:10 +0100

    Added JSTree stuff

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,37 @@
 (function ($) {
   'use strict';
 
+  var tree;
+
   var renderDataOnPage = function (data) {
       var container = $('#data');
       var units = data['Units Table'];
 
-      var html = units.reduce(function (agg, next) {
-        agg += '<li>Uid: ' + next.uid + ' - hwaddr: ' + next.hwaddr + '</li>';
-        return agg;
-      }, '');
+      container.empty();
 
-      container.html(html);
-  };
+      var mapped = units.map(function (unit) {
+        return {
+          text: unit.uid,
+          state: {
+            opened: true
+          }
+        };
+      });
+
+      container.jstree({
+          core: {
+            data: mapped
+          }
+        });  };
 
   var getData = function () {
     $.get('/data/units_table.json?foo=' + Math.random()).then(renderDataOnPage);
   };
 
   var init = function () {
-    getData();
 
-    setInterval(getData, 300);
+    getData();
+    //setInterval(getData, 300);
   };
 
   $(document).ready(init);
